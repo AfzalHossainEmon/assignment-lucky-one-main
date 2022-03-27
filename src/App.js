226aa51ab02b0ff49156adf32751c7a64b2fe44f
AfzalHossainEmon <a.hossainemon7@gmail.com>
2022-03-27 19:13:54 +0600

    new main

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Choose from './component/Choose/Choose';
+
+function App() {
+  return (
+    <div className="App">
+      <Choose></Choose>
+    </div>
+  );
+}
+
+export default App;
